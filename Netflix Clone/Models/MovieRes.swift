15aabe874853628df485f60b45d7367f7e2e9a9c
Iman Zabihi <iman@smedia.com.au>
2022-08-09 17:46:47 +0800

    Update UI.

---
+++ Netflix Clone/Models/MovieRes.swift
@@ -0,0 +1,49 @@
+//
+//  MovieRes.swift
+//  Netflix Clone
+//
+//  Created by Iman Zabihi on 08/08/2022.
+//
+
+import Foundation
+
+// MARK: - Result
+struct MovieRes: Codable {
+    let adult: Bool
+    let backdropPath: String
+    let id: Int
+    let title: String?
+    let originalLanguage: String
+    let originalTitle: String?
+    let overview, posterPath: String?
+    let mediaType: String?
+    let genreIDS: [Int]
+    let popularity: Double
+    let releaseDate: String?
+    let video: Bool?
+    let voteAverage: Double
+    let voteCount: Int
+    let name, originalName, firstAirDate: String?
+    let originCountry: [String]?
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case id, title
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview
+        case posterPath = "poster_path"
+        case mediaType = "media_type"
+        case genreIDS = "genre_ids"
+        case popularity
+        case releaseDate = "release_date"
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+        case name
+        case originalName = "original_name"
+        case firstAirDate = "first_air_date"
+        case originCountry = "origin_country"
+    }
+}
